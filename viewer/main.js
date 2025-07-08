195e90524bf723d49b9789be34ac38ee985ef872
Youjin Lee <ol-of@naver.com>
2025-07-08 16:59:39 +0900

    Sync

--- viewer/main.js
+++ viewer/main.js
@@ -160,7 +160,6 @@ function initialize() {
         console.log('Stats checkbox is now', checkbox.checked ? 'checked' : 'unchecked');
         checkbox.dispatchEvent(new Event('change', { bubbles: true }));
     });
-    /*document.getElementById("console-bottom-scroll").addEventListener("click", onConsoleBottom, false);*/
 
     document.getElementById("zoom-slider").addEventListener("input", onZoomSlider, false);
     document.getElementById("zoom-value").addEventListener("keydown", onZoomValue, false);
@@ -189,28 +188,6 @@ function initialize() {
             actions.style.right = '';
         }
     });
-}
-
-function openDrawer() {
-    const drawer = document.querySelector('.drawer');
-    const backdrop = document.getElementById('drawer-backdrop');
-    const actions = document.querySelector('.actions');
-    drawer.classList.add('open');
-    backdrop.classList.add('show');
-    if (window.innerWidth <= 1023) {
-        actions.style.right = '340px';
-    }
-}
-
-function closeDrawer() {
-    const drawer = document.querySelector('.drawer');
-    const backdrop = document.getElementById('drawer-backdrop');
-    const actions = document.querySelector('.actions');
-    drawer.classList.remove('open');
-    backdrop.classList.remove('show');
-    if (window.innerWidth <= 1023) {
-        actions.style.right = '80px';
-    }
 }
 
 function openFileBrowse() {
@@ -292,7 +269,6 @@ function loadData(data, fileExtension) {
     setTimeout(async () => {
         await player.load(data, fileExtension);
         resize(size, size);
-        /*showAside();*/
         createTabs();
         showImageCanvas();
         createFilesListTab();
@@ -345,27 +321,6 @@ function createFilesListTab() {
     }
 }
 
-/*
-function showAside() {
-    var aside = document.getElementsByTagName("aside")[0];
-    aside.classList.remove("hidden");
-}
-
-function showPage(name) {
-    showAside();
-    var aside = document.getElementsByTagName("aside")[0];
-    var tabs = aside.getElementsByClassName("tab");
-    for (let tab of tabs) {
-        tab.classList.toggle("active", tab.id === name);
-    }
-    var nav = aside.getElementsByTagName("nav")[0];
-    var navChilds = nav.childNodes;
-    for (let child of navChilds) {
-        if (child.tagName === "A")
-            child.classList.toggle("active", child.id === "nav-" + name);
-    }
-}*/
-
 // Main image section
 function showImageCanvas() {
     var placeholder = document.getElementById("image-placeholder");
@@ -396,28 +351,6 @@ function resize(width, height) {
     player.style.height = `${height}px`;
     player.resize(width, height);
 }
-
-/*
-function onToggleAside() {
-    var aside = document.getElementsByTagName("aside")[0];
-    aside.classList.toggle("hidden");
-}
-*/
-/*
-function onShowProgress() {
-    showPage("progress");
-}
-*/
-/*
-function onShowFile() {
-    showPage("file");
-}
-*/
-/*
-function onShowFilesList() {
-    showPage("files-list");
-}
-*/
 
 function onDarkMode(event) {
     document.body.classList.toggle("dark-mode", event.target.checked);
@@ -698,4 +631,26 @@ function refreshZoomValue() {
     var value = document.getElementById("zoom-value");
     value.innerHTML = player.offsetWidth + " x " + player.offsetHeight;
     value.classList.remove("incorrect");
+}
+
+function openDrawer() {
+    const drawer = document.querySelector('.drawer');
+    const backdrop = document.getElementById('drawer-backdrop');
+    const actions = document.querySelector('.actions');
+    drawer.classList.add('open');
+    backdrop.classList.add('show');
+    if (window.innerWidth <= 1023) {
+        actions.style.right = '340px';
+    }
+}
+
+function closeDrawer() {
+    const drawer = document.querySelector('.drawer');
+    const backdrop = document.getElementById('drawer-backdrop');
+    const actions = document.querySelector('.actions');
+    drawer.classList.remove('open');
+    backdrop.classList.remove('show');
+    if (window.innerWidth <= 1023) {
+        actions.style.right = '80px';
+    }
 }
